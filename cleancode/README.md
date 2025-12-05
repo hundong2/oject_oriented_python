7174bfec0e1096cbe312165c2c958ad838829965
hundong2 <ehdgns104@gmail.com>
2025-12-05 23:34:27 +0900

    Update Python example for modernize

---
+++ cleancode/README.md
@@ -0,0 +1,8 @@
+# install 
+
+```sh
+uv init 
+uv venv 
+source .venv/bin/activate
+```
+
---
+++ cleancode/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from cleancode!")
+
+
+if __name__ == "__main__":
+    main()
